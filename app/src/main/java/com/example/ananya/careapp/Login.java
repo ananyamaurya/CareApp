6a44a6b02598c023054095e917558dea51a44018
Ananya Maurya <ananya2531997@outlook.com>
2018-02-23 14:29:12 +0530

    Login/Register

--- app/src/main/java/com/example/ananya/careapp/Login.java
+++ app/src/main/java/com/example/ananya/careapp/Login.java
@@ -11,6 +11,8 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
         getSupportActionBar().show();
+
     }
 }
