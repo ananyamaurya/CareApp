8ec0a942d21acb8e00bf8331415a3e5259f7bd28
Ananya Maurya <ananya2531997@outlook.com>
2018-02-23 15:06:17 +0530

    Login/Register

--- app/src/main/java/com/example/ananya/careapp/Login.java
+++ app/src/main/java/com/example/ananya/careapp/Login.java
@@ -2,17 +2,29 @@ package com.example.ananya.careapp;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 
 public class Login extends AppCompatActivity {
-
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().show();
+        login =findViewById(R.id.button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),Register.class);
+                startActivity(i);
+
+            }
+        });
 
     }
 }
