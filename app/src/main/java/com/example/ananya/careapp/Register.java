930965de07f1d7e4ff31d550c7981da45da86fa1
Ananya Maurya <ananya2531997@outlook.com>
2018-03-10 11:34:34 +0530

    Send Verfication Change

--- app/src/main/java/com/example/ananya/careapp/Register.java
+++ app/src/main/java/com/example/ananya/careapp/Register.java
@@ -77,7 +77,7 @@ public class Register extends AppCompatActivity {
                                             }
                                         }
                                     });
-                            sendVerificationEmail();
+
                         }
 
                         // ...
