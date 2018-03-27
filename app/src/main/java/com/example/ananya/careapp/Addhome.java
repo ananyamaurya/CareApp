f86a21619f622f80608e7cd025d36aa9aee07444
Ananya Maurya <ananya2531997@outlook.com>
2018-03-27 09:41:58 +0530

    HomeViewFlipper

--- app/src/main/java/com/example/ananya/careapp/Addhome.java
+++ app/src/main/java/com/example/ananya/careapp/Addhome.java
@@ -28,8 +28,7 @@ public class Addhome extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v= inflater.inflate(R.layout.fragment_addhome, container, false);
-        return v;
+         return inflater.inflate(R.layout.fragment_addhome, container, false);
     }
 
 }
